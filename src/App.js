8e740a83ba79de9366c856546fd627523f04041b
Roman Babushkin <romariobabushkin@gmail.com>
2021-03-18 16:09:19 +0300

    Изменил App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const someWord = "I realy like how JSX works!";
+const truth = true;
+const situation = false;
+
 function App() {
   return (
     <div className="App">
@@ -9,8 +13,26 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{color: 'yellow'}}>
+          Hello world!
+        </p>
         <p>
-          Hello world, i'm ready!
+          {someWord}
+        </p>
+        <p>
+          {23}    
+        </p>
+        <p>
+          {23+50}    
+        </p>
+        <p>
+          {truth&&"Okey, this is turth!"}    
+        </p>
+        <p>
+          {situation ? "This is turth!":"This one can be false;("}    
+        </p>
+        <p>
+          undefined: {undefined}, null: {null}, false: {false}, true: {true}
         </p>
         <a
           className="App-link"
