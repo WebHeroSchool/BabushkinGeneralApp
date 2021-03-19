ef36e1efe451df28498cb9f511e1d65847a000f8
Roman Babushkin <romariobabushkin@gmail.com>
2021-03-19 16:09:05 +0300

    Добавил стили

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import './App.css'
 
 const countDeal = 10;
 const todoItem = [{
@@ -11,10 +12,10 @@ const todoItem = [{
   }, {
     value: 'Развиваться в разработке дальше!'
   }];
-  
+
 const App = () =>  (
-    <div>
-<h1>Очень важные дела!</h1>
+    <div className="wrap">
+<h1 className="wrap__title">Очень важные дела!</h1>
 <InputItem/>
 <ItemList todoItem={todoItem}/>
 <Footer count={countDeal}/></div>);
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './Footer.css'
 
-const Footer = ({ count }) => (<span>Осталось выполнить {count} дел</span>)
+const Footer = ({ count }) => (<span className="wrap__footer">Осталось выполнить {count} дел</span>)
 
 export default Footer;
