c9f3fc5f32e75f98c55f30b3ebd072cc85afe9fe
Roman Babushkin <romariobabushkin@gmail.com>
2021-03-22 15:16:21 +0300

    Добавил компонент Input из библиотеки

--- src/Components/InputItem/InputItem.js
+++ src/Components/InputItem/InputItem.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<span>Тут будет Input</span>)
+const InputItem = () => (<div><TextField
+          id="outlined-full-width"
+          label="Добавь дело"
+          style={{ margin: 8 }}
+          placeholder="Дело"
+          helperText="Добавь важное дело!"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        /></div>)
 
 export default InputItem;
