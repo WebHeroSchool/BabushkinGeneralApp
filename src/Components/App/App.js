61f8a6b4dce6c39f1eda89f23d1237662fe1596b
Roman Babushkin <romariobabushkin@gmail.com>
2021-03-23 15:39:51 +0300

    Пернес items внуть state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,33 +7,38 @@ import styles from './App.module.css';
 const countDeal = 3;
 
 class App extends React.Component {
-  render() {
-     const items = [{
-    value: 'Изучить HTML',
-    isDone: true
-  }, {
-    value: 'Изучить CSS', 
-    isDone: true
-    
-  }, {
-    value: 'Изучить JavaScript', 
-    isDone: false
-  }, {
-    value: 'Изучить React', 
-    isDone: false
-  }
-  , {
-    value: 'Найти работу мечты', 
-    isDone: false
-  }];
+  state = {
+    items: [{
+        value: 'Изучить HTML',
+        isDone: true
+      },
+      {
+        value: 'Изучить CSS',
+        isDone: true
 
-  return (
-    <div className={styles.wrap}>
-<h1 className={styles.title}>Очень важные дела!</h1>
-<InputItem/>
-<ItemList items={items}/>
-<Footer count={countDeal}/></div>);
+      },
+      {
+        value: 'Изучить JavaScript',
+        isDone: false
+      },
+      {
+        value: 'Изучить React',
+        isDone: false
+      },
+      {
+        value: 'Найти работу мечты',
+        isDone: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className={styles.wrap}>
+      <h1 className={styles.title}>Очень важные дела!</h1>
+      <InputItem/>
+      <ItemList items={this.state.items}/>
+      <Footer count={countDeal}/></div>);
   }
-}
+};
 
 export default App;
